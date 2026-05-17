156d2f686f5b8b246fedec1e46e0e43df22b4061
kath <kath45823@gmail.com>
2026-05-17 10:43:29 -0700

    feat: bertopic modeling done

--- src/features/scores.py
+++ src/features/scores.py
@@ -3,52 +3,56 @@ from impact_factor.core import Factor
 from datetime import date, datetime
 import numpy as np
 
-papers = fetch_papers()
-scores = []
-fa = Factor()
-scored_papers = []
+def filtered_papers():
+    papers = fetch_papers()
+    scores = []
+    fa = Factor()
+    scored_papers = []
 
-for paper in papers:
-    # Scoring 
-    results = fa.search(paper["journal"])
-    if not results: 
-        continue
-    
-    jif = results[0]["factor"]
+    for paper in papers:
+        # Scoring 
+        results = fa.search(paper["journal"])
+        if not results: 
+            continue
+        
+        jif = results[0]["factor"]
 
-    p_date = paper["publication_date"]
-    if "Year" not in p_date:
-        continue
-    
-    if "Month" in p_date:
-        try: 
-            month_num = datetime.strptime(p_date["Month"], "%B").month
-        except: 
+        p_date = paper["publication_date"]
+        if "Year" not in p_date:
+            continue
+        
+        if "Month" in p_date:
             try: 
-                month_num = int(p_date["Month"])
-            except:
-                month_num = 1
-    else: 
-        month_num = 1
+                month_num = datetime.strptime(p_date["Month"], "%B").month
+            except: 
+                try: 
+                    month_num = int(p_date["Month"])
+                except:
+                    month_num = 1
+        else: 
+            month_num = 1
 
-    if "Day" in p_date:
-        day = int(p_date["Day"])
-    else: 
-        day = 1
+        if "Day" in p_date:
+            day = int(p_date["Day"])
+        else: 
+            day = 1
+        
+        pub_date = date(int(p_date["Year"]), month_num, day)
+        days_since_pub = (datetime.now().date() - pub_date).days
+
+        score = 0.7 * jif + 0.3 * (1 / (days_since_pub + 1)) 
+        paper["score"] = score
+        scores.append(score)
+        scored_papers.append(paper)
+
+    # Filter out lower score papers
+    threshold = np.percentile(scores, 70)   # experiment on percentile later
+    highest_scores_papers = []
+
+    for paper in scored_papers: 
+        score = paper["score"]
+
+        if score >= threshold:
+            highest_scores_papers.append(paper)
     
-    pub_date = date(int(p_date["Year"]), month_num, day)
-    days_since_pub = (datetime.now().date() - pub_date).days
-
-    score = 0.7 * jif + 0.3 * (1 / (days_since_pub + 1))   # maybe normalize later + experiment with weights 
-    paper["score"] = score
-    scores.append(score)
-    scored_papers.append(paper)
-
-threshold = np.percentile(scores, 70)   # experiment on percentile later
-highest_scores_papers = []
-
-for paper in scored_papers: 
-    score = paper["score"]
-
-    if score >= threshold:
-        highest_scores_papers.append(paper)
+    return highest_scores_papers
---
+++ src/models/topic_model.py
@@ -0,0 +1,18 @@
+from src.features.scores import filtered_papers
+from bertopic.representation import ZeroShotClassification
+from bertopic import BERTopic
+
+papers = filtered_papers()
+mesh_tags = []
+abstracts = []
+
+for paper in papers:
+    mesh_tags.append(paper["mesh_tags"])
+    abstracts.append(paper["abstract"])
+
+mesh_tags = [t for tags in mesh_tags for t in tags]
+
+representation_model = ZeroShotClassification(mesh_tags, model="facebook/bart-large-mnli")
+topic_model = BERTopic(representation_model = representation_model)
+topics, prob = topic_model.fit_transform(abstracts)
+print(topic_model.get_topic_info())
