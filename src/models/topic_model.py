8e623181cd72faa7eec18e2b9ba63a9e8e7524a3
kath <kath45823@gmail.com>
2026-05-17 17:47:28 -0700

    fix: fine tuned bertopic model for better topic clusters

--- src/models/topic_model.py
+++ src/models/topic_model.py
@@ -1,6 +1,9 @@
 from src.features.scores import filtered_papers
 from bertopic.representation import ZeroShotClassification
 from bertopic import BERTopic
+from umap import UMAP
+from hdbscan import HDBSCAN
+from sklearn.feature_extraction.text import CountVectorizer
 
 papers = filtered_papers()
 mesh_tags = []
@@ -11,10 +14,33 @@ for paper in papers:
     abstracts.append(paper["abstract"])
 
 mesh_tags = [t for tags in mesh_tags for t in tags]
-
 representation_model = ZeroShotClassification(mesh_tags, model="facebook/bart-large-mnli")
-topic_model = BERTopic(representation_model = representation_model)
-topic_model = BERTopic()
+
+umap_model = UMAP(
+    n_neighbors=15,
+    n_components=5,
+    min_dist=0.0,
+    metric="cosine",
+    random_state=42  
+)
+
+hdbscan_model = HDBSCAN(
+    min_cluster_size=5,
+    min_samples=2,
+    metric="euclidean",
+    cluster_selection_method="eom"
+)
+
+vectorizer = CountVectorizer(stop_words="english")
+
+topic_model = BERTopic(
+    umap_model=umap_model,
+    hdbscan_model=hdbscan_model,
+    vectorizer_model=vectorizer,
+    representation_model=representation_model,
+    calculate_probabilities=False
+)
 topics, prob = topic_model.fit_transform(abstracts)
 
 print(topic_model.get_topic_info())
+print(topic_model.get_topic_info()["Name"].tolist())
