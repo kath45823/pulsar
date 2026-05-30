8e062f44d81a72fd81266e21c43a63d2f76dd86e
kath <kath45823@gmail.com>
2026-05-30 14:19:38 -0700

    fix: truncated abstracts to 512 tokens

--- src/models/ner_model.py
+++ src/models/ner_model.py
@@ -2,13 +2,23 @@ from transformers import pipeline
 import re
 from src.models.topic_model import get_papers_per_topic
 from collections import Counter
+from transformers import AutoTokenizer
 
-diseases_ner = pipeline("ner", model="OpenMed/OpenMed-NER-DiseaseDetect-BioMed-335M", aggregation_strategy = "simple")
-cell_ner = pipeline("ner", model="siddharthtumre/biobert-finetuned-ner", aggregation_strategy = "simple")
-chem_ner = pipeline("ner", model="OpenMed/OpenMed-NER-ChemicalDetect-PubMed-335M", aggregation_strategy = "first")
-gene_ner = pipeline("ner", model="pruas/BENT-PubMedBERT-NER-Gene", aggregation_strategy = "first")
+def init():
+    disease_tokenizer = AutoTokenizer.from_pretrained("OpenMed/OpenMed-NER-DiseaseDetect-BioMed-335M")
+    cell_tokenizer = AutoTokenizer.from_pretrained("siddharthtumre/biobert-finetuned-ner")
+    chem_tokenizer = AutoTokenizer.from_pretrained("OpenMed/OpenMed-NER-ChemicalDetect-PubMed-335M")
+    gene_tokenizer = AutoTokenizer.from_pretrained("pruas/BENT-PubMedBERT-NER-Gene")
+
+    diseases_ner = pipeline("ner", model="OpenMed/OpenMed-NER-DiseaseDetect-BioMed-335M", aggregation_strategy = "simple")
+    cell_ner = pipeline("ner", model="siddharthtumre/biobert-finetuned-ner", aggregation_strategy = "simple")
+    chem_ner = pipeline("ner", model="OpenMed/OpenMed-NER-ChemicalDetect-PubMed-335M", aggregation_strategy = "first")
+    gene_ner = pipeline("ner", model="pruas/BENT-PubMedBERT-NER-Gene", aggregation_strategy = "first")
+
+    return diseases_ner, cell_ner, chem_ner, gene_ner, disease_tokenizer, cell_tokenizer, chem_tokenizer, gene_tokenizer
 
 def clean(word):
+    word = re.sub(r'<[^>]+>', '', word)
     word = re.sub(r'\s*-\s*', '-', word)
     word = re.sub(r'\s*cell\s*(line|type|lines|types).*', '', word, flags=re.IGNORECASE)
     word = re.sub(r'^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+$', '', word)
@@ -17,7 +27,16 @@ def clean(word):
 
     return word.strip()
 
-def run_ner_on_abstract(abstract):
+def truncate(abstract, tokenizer):
+    tokens = tokenizer(
+        abstract,
+        max_length=512,
+        truncation=True,
+        return_tensors=None
+    )
+    return tokenizer.decode(tokens["input_ids"], skip_special_tokens=True)
+
+def run_ner_on_abstract(abstract, diseases_ner, cell_ner, chem_ner, gene_ner, disease_tokenizer, cell_tokenizer, chem_tokenizer, gene_tokenizer):
     entities = {
         "chemicals": [],
         "diseases": [],
@@ -26,31 +45,33 @@ def run_ner_on_abstract(abstract):
         "cell_lines": []
     }
 
-    for entity in diseases_ner(abstract):
-        if entity["score"] >= 0.9 and entity["entity_group"] == "DISEASE":
+    for entity in diseases_ner(truncate(abstract, disease_tokenizer)):
+        if entity["score"] >= 0.95 and entity["entity_group"] == "DISEASE":
             entities["diseases"].append(clean(entity["word"]))
     
-    for entity in cell_ner(abstract):
+    for entity in cell_ner(truncate(abstract, cell_tokenizer)):
         if entity["score"] >= 0.9:
             if entity["entity_group"] == "cell_type":
                 entities["cell_types"].append(clean(entity["word"]))
             elif entity["entity_group"] == "cell_line":
                 entities["cell_lines"].append(clean(entity["word"]))
     
-    for entity in chem_ner(abstract):
+    for entity in chem_ner(truncate(abstract, chem_tokenizer)):
         if entity["score"] >= 0.9 and entity["entity_group"] == "CHEM":
             entities["chemicals"].append(clean(entity["word"]))
 
-    for entity in gene_ner(abstract):
+    for entity in gene_ner(truncate(abstract, gene_tokenizer)):
         if entity["score"] >= 0.9 and entity["entity_group"] == "B":
             entities["genes/proteins"].append(clean(entity["word"]))
 
     for e_type in entities:
-        entities[e_type] = list(set(entities[e_type]))
+        entities[e_type] = list(set(e for e in entities[e_type] if e and len(e) > 1))
     
     return entities
 
 def topic_ner():
+    diseases_ner, cell_ner, chem_ner, gene_ner, disease_tokenizer, cell_tokenizer, chem_tokenizer, gene_tokenizer = init()
+
     topic_papers = get_papers_per_topic()
     topic_ner_tags = {}
 
@@ -63,7 +84,7 @@ def topic_ner():
             "cell_lines": []
         }
         for paper in papers: 
-            paper_tags = run_ner_on_abstract(paper["abstract"])
+            paper_tags = run_ner_on_abstract(paper["abstract"], diseases_ner, cell_ner, chem_ner, gene_ner, disease_tokenizer, cell_tokenizer, chem_tokenizer, gene_tokenizer)
             all_ner_tags["chemicals"].extend(paper_tags["chemicals"])
             all_ner_tags["diseases"].extend(paper_tags["diseases"])
             all_ner_tags["genes/proteins"].extend(paper_tags["genes/proteins"])
