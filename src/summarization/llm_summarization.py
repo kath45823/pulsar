600ba43e9b2b18502cddec3cd127cf40c65b896b
kath <kath45823@gmail.com>
2026-05-30 15:25:59 -0700

    feat: generate llm summaries

--- src/summarization/llm_summarization.py
+++ src/summarization/llm_summarization.py
@@ -1,9 +1,51 @@
 from src.models.ner_model import run_ner_on_topics
+from google import genai
+from dotenv import load_dotenv
+import os
 
-def summarize_topic():
-    return 
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def summarize_all_topics():
     topic_papers_tags = run_ner_on_topics()
-    for topic in topic_papers_tags:
-        return 
+    topic_summaries = {}
+
+    for topic, topic_item in topic_papers_tags.items():
+        title = topic
+        count = topic_item["paper_count"]
+        chem = topic_item["tags"]["chemicals"]
+        diseases = topic_item["tags"]["diseases"]
+        genes = topic_item["tags"]["genes/proteins"]
+        cell_types = topic_item["tags"]["cell_types"]
+        cell_lines = topic_item["tags"]["cell_lines"]
+        rep_abstracts = "\n\n".join(topic_item["representative_abstracts"])
+
+        prompt = f"""
+            Topic: {title}
+            Papers this week: {count}
+
+            Key entities:
+            - Chemicals: {chem}
+            - Diseases: {diseases}  
+            - Genes/Proteins: {genes}
+            - Cell types: {cell_types}
+            - Cell lines: {cell_lines}
+
+            Representative abstracts:
+            {rep_abstracts}
+
+            Write a 3-4 sentence summary of what research is saying about 
+            this topic this week. Be specific, reference key entities
+        """
+        
+        response = client.models.generate_content(
+            model="gemini-3.5-flash",
+            contents=prompt,
+        )
+
+        topic_summaries[title] = {
+            "tags": topic_item["tags"],
+            "summary": response.text
+        }
+
+    return topic_summaries
